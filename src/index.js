9b1a17e05d9e607ae9d77c4fe5efda90ec50c63d
KylixMedusa <ayush.agarwalmng@gmail.com>
2022-01-15 16:08:39 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/creatorpad">
+    <BrowserRouter basename="/creator-pad">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
